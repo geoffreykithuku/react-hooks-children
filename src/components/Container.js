4437cd7b947ffbb43c8572a4c37df68ee344f9de
geoffreykithuku <mutembeikithuku12@gmail.com>
2022-09-18 23:23:02 +0300

    Completed Assignment

--- src/components/Container.js
+++ src/components/Container.js
@@ -9,6 +9,7 @@ function Container({
 }) {
   return (
     <div className={`container ${textPosition}`}>
+      
       {header ? <h2>{header}</h2> : null}
 
       <div className={`container-children ${direction} ${contentPosition}`}>
